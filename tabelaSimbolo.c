207b1469e39e9073e747c76f2fa175cd908ad9be
Leonardo Lana <leo0198@gmail.com>
2016-11-01 21:05:24 -0200

    Add the main function

---
+++ tabelaSimbolo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctypes.h>
+
+int main (int argc, char **argv) {
+
+	int c;
+	char *tipo, *modo;
+	FILE *entrada;
+
+	entrada = fopen (argv[1], "r");
+	tipo = argv[2];
+	modo = argv[3];
+
+	return 0;
+}
